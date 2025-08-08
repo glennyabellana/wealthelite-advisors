9f3529c6d59c95830170b615351c705d1b0b5d4e
Glenny Abellana <glenny.abellana@gmail.com>
2025-08-09 01:16:19 +0800

    refactor functions.php to properly enqueue styles on npm build

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,31 +1,34 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => ({
-  root: 'src',
-  base: command === 'serve' ? '/' : '/wp-content/themes/wealthelite-advisors/dist/',
-  build: {
-    manifest: true,
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: 'src/js/index.js',
-        style: 'src/scss/style.scss',
-      },
-    },
-  },
-  server: {
-    host: true,
-    port: 3000,
-    cors: true,
-    allowedHosts: ['localhost', 'wealtheliteadvisors.test'],
-    proxy: {
-      '^/(?!(?:@vite/|@fs/|@id/|node_modules/|js/|css/|scss/|assets/|favicon\.ico))': {
-        target: 'http://wealtheliteadvisors.test',
-        changeOrigin: false,
-        xfwd: true,
-        secure: false,
-      },
-    },
-  },
+	root: 'src',
+	base:
+		command === 'serve'
+			? '/'
+			: '/wp-content/themes/wealthelite-advisors/dist/',
+	build: {
+		manifest: true,
+		outDir: '../dist',
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: 'src/js/index.js',
+			},
+		},
+	},
+	server: {
+		host: true,
+		port: 3000,
+		cors: true,
+		allowedHosts: ['localhost', 'wealtheliteadvisors.test'],
+		proxy: {
+			'^/(?!(?:@vite/|@fs/|@id/|node_modules/|js/|css/|scss/|assets/|favicon.ico))':
+				{
+					target: 'http://wealtheliteadvisors.test',
+					changeOrigin: false,
+					xfwd: true,
+					secure: false,
+				},
+		},
+	},
 }));
